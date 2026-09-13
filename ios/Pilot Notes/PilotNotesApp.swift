5fd4c5b7880097d91f6150a35f6a1564b41057a4
Artem Zasypalov <gaoyundexinmen@gmail.com>
2026-09-13 12:05:07 +0200

    iOS Project init

---
+++ ios/Pilot Notes/PilotNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pilot_NotesApp.swift
+//  Pilot Notes
+//
+//  Created by Artem Zasypalov on 13.09.26.
+//
+
+import SwiftUI
+
+@main
+struct PilotNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
